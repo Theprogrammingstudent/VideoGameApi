63d9259f0c48f6f278cc9c27c718f81c90a908e2
alex-mulkerrins <D21126666@mytudublin.ie>
2025-01-26 23:24:11 +0000

    video Game Web API

---
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+
+        static private List<VideoGame> videoGames = new List<VideoGame>
+        {
+            new VideoGame
+            {
+                Id = 1,
+                Title = "Spider-Man 2",
+                Platform = "PS5",
+                Developer = "Insomniac Games",
+                Publisher = "Sony Interactive Entertainment"
+            },
+            new VideoGame
+            {
+                Id = 2,
+                Title = "The Legend of Zelda: Breadth of the Wild",
+                Platform = "Nintendo Switch",
+                Developer = "Nintendo EPD",
+                Publisher = "Nintendo"
+            },
+            new VideoGame
+            {
+                Id = 3,
+                Title = "Cyberpunk 2077 2",
+                Platform = "PC",
+                Developer = "CD Projekt Red",
+                Publisher = "CD Projekt"
+            }
+
+        };
+
+        [HttpGet]
+        public ActionResult<List<VideoGame>> GetVideoGames()
+        {
+            return Ok(videoGames);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<VideoGame> GetVideoGameByID(int id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+                return NotFound();
+
+            return Ok(game);
+        }
+
+        //[HttpPost]
+        //public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
+        //{
+        //    if (newGame is null)
+        //        return BadRequest();
+
+            
+        //}
+
+    }
+}
