20179727d2f0ede3b654f62e35e6fe744c2b7121
alex-mulkerrins <D21126666@mytudublin.ie>
2025-01-29 22:46:35 +0000

    video game API updates

--- VideoGameApi/Controllers/VideoGameController.cs
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -45,7 +45,7 @@ namespace VideoGameApi.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<VideoGame> GetVideoGameByID(int id)
+        public ActionResult<VideoGame> GetVideoGameById(int id)
         {
             var game = videoGames.FirstOrDefault(g => g.Id == id);
             if (game is null)
@@ -54,14 +54,45 @@ namespace VideoGameApi.Controllers
             return Ok(game);
         }
 
-        //[HttpPost]
-        //public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
-        //{
-        //    if (newGame is null)
-        //        return BadRequest();
+        [HttpPost]
+        public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
+        {
+            if (newGame is null)
+                return BadRequest();
 
-            
-        //}
+            newGame.Id = videoGames.Max(g => g.Id) + 1;
+            videoGames.Add(newGame);
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateVideoGame(int id, VideoGame updatesGame)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+                return NotFound();
+
+            game.Title = updatesGame.Title;
+            game.Platform = updatesGame.Platform;
+            game.Developer = updatesGame.Developer;
+            game.Publisher = updatesGame.Publisher;
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteVideoGame(int id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+
+            if (game is null)
+                return NotFound();
+
+            videoGames.Remove(game);
+            return NoContent();
+
+        }
 
     }
 }
---
+++ VideoGameApi/Data/VideoGameDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VideoGameApi.Data
+{
+    public class VideoGameDBContext(DbContextOptions<VideoGameDBContext> options) : DbContext(options)
+    {
+        public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+    }
+}
